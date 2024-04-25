b08f62443a4088d9a13475a9e5b8e507e6be9ec6
arima-naoto <k023g1010@g.neec.ac.jp>
2024-04-25 12:50:07 +0900

    Renderingクラスの追加

---
+++ DirectXGame/Rendering.cpp
@@ -0,0 +1,141 @@
+#include "Rendering.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+/// <summary>
+/// 行列の積
+/// </summary>
+/// <param name="m1"></param>
+/// <param name="m2"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 MultiplyMatrix{};
+
+	//for分を使用して、行列の積を求める
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			MultiplyMatrix.m[i][j] = 0;
+			for (int k = 0; k < 4; k++)
+			{
+				MultiplyMatrix.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+
+	return MultiplyMatrix;
+}
+
+/// <summary>
+/// 拡大縮小行列
+/// </summary>
+/// <param name="scale"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeScaleMatrix(const Vector3& scale)
+{
+	
+	Matrix4x4 resultScale = {
+		scale.x,0.0f,0.0f,0.0f,
+		0.0f,scale.y,0.0f,0.0f,
+		0.0f,0.0f,scale.z,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	return resultScale;
+}
+
+/// <summary>
+/// X軸回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeRotateXMatrix(float radian)
+{
+	Matrix4x4 rotateXMatrix = {
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,std::cos(radian),std::sin(radian),0.0f,
+		0.0f,-std::sin(radian),std::cos(radian),0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	return rotateXMatrix;
+}
+
+/// <summary>
+/// Y軸回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeRotateYMatrix(float radian)
+{
+	Matrix4x4 rotateYMatrix = {
+		std::cos(radian),0.0f,-std::sin(radian),0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		std::sin(radian),0.0f,std::cos(radian),0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	return rotateYMatrix;
+}
+
+/// <summary>
+/// Z軸回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeRotateZMatrix(float radian)
+{
+	Matrix4x4 rotateZMatrix = {
+		std::cos(radian),std::sin(radian),0.0f,0.0f,
+		-std::sin(radian),std::cos(radian),0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	return rotateZMatrix;
+}
+
+/// <summary>
+/// 回転行列
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeRotateMatrix(const Vector3& radian)
+{
+	//行列の積を使用して、X軸・Y軸・Z軸回転行列を結合する
+	return Multiply(MakeRotateXMatrix(radian.z), Multiply(MakeRotateYMatrix(radian.y), MakeRotateZMatrix(radian.x)));
+}
+
+/// <summary>
+/// 平行移動行列
+/// </summary>
+/// <param name="translate"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 resultTranslate = {
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		translate.x,translate.y,translate.z,1.0f
+	};
+
+	return resultTranslate;
+}
+
+/// <summary>
+/// アフィン変換行列
+/// </summary>
+/// <param name="affine"></param>
+/// <returns></returns>
+Matrix4x4 Rendering::MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate)
+{
+	//行列の積を使用し、拡大縮小行列・回転行列・平行移動行列を結合する
+	return Multiply(Multiply(MakeScaleMatrix(scale),MakeRotateMatrix(rotate)),MakeTranslateMatrix(translate));
+}
+
+
+
+
+
+
---
+++ DirectXGame/Rendering.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "Matrix4x4.h"
+#include "Vector3.h"
+
+/// <summary>
+/// Renderingクラスの作成
+/// </summary>
+class Rendering
+{
+public://メンバ関数の作成
+
+#pragma region 4x4行列メンバ関数の作成
+
+	/// <summary>
+	/// 行列の積
+	/// </summary>
+	/// <param name="m1"></param>
+	/// <param name="m2"></param>
+	/// <returns></returns>
+	static Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+	/// <summary>
+	/// 拡大縮小行列
+	/// </summary>
+	/// <param name="scale"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+	/// <summary>
+	/// X軸回転行列
+	/// </summary>
+	/// <param name="radian"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeRotateXMatrix(float radian);
+
+	/// <summary>
+	/// Y軸回転行列
+	/// </summary>
+	/// <param name="radian"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeRotateYMatrix(float radian);
+
+	/// <summary>
+	/// Z軸回転行列
+	/// </summary>
+	/// <param name="radian"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeRotateZMatrix(float radian);
+
+	/// <summary>
+	/// 回転行列
+	/// </summary>
+	/// <param name="radian"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeRotateMatrix(const Vector3& radian);
+
+	/// <summary>
+	/// 平行移動行列
+	/// </summary>
+	/// <param name="translate"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+	/// <summary>
+	/// アフィン変換行列
+	/// </summary>
+	/// <param name="affine"></param>
+	/// <returns></returns>
+	static Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+	
+};
+
